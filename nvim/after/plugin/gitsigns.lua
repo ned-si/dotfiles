c22018c528f6e06ae3c33af797dc4e45034f58f9
Julien Sudan <nedsi@pm.me>
2023-01-06 22:03:07 +0100

    feat: add descriptions for gitsigns (hunks)

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -22,27 +22,27 @@ require('gitsigns').setup {
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, {expr=true, desc = "[H]unk [N]ext" })
 
     map('n', '<leader>hp', function()
       if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, {expr=true, desc = "[H]unk [P]revious"})
 
     -- Actions
-    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', gs.stage_buffer, {desc = '[H]unk [S]tage buffer'}) -- TODO: find how to add desc to those remaps!
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hP', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>ht', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>htd', gs.toggle_deleted)
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = '[H]unk - [S]tage hunk' })
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = '[H]unk - [R]eset hunk' })
+    map('n', '<leader>hS', gs.stage_buffer, { desc = '[H]unk - [S]tage buffer' })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = '[H]unk - [U]ndo staged hunk' })
+    map('n', '<leader>hR', gs.reset_buffer, { desc = '[H]unk - [R]eset buffer' })
+    map('n', '<leader>hP', gs.preview_hunk, { desc = '[H]unk - [P]review hunk' })
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = '[H]unk - [B]lame line' })
+    map('n', '<leader>ht', gs.toggle_current_line_blame, { desc = '[H]unk - [T]oggle blame line' })
+    map('n', '<leader>hd', gs.diffthis, { desc = '[H]unk - [D]iff' })
+    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = '[H]unk - [D]iff with HEAD' })
+    map('n', '<leader>htd', gs.toggle_deleted, { desc = '[H]unk - [T]oggle [D]eleted lines' })
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[I]n [H]unk' })
   end }
