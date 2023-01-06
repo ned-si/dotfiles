7411a36c97ad9e3ad1d665f340e1a408885df7ee
Julien Sudan <nedsi@pm.me>
2023-01-06 23:58:55 +0100

    chore: update TODO mentions

--- nvim/lua/nedsi/remap.lua
+++ nvim/lua/nedsi/remap.lua
@@ -5,7 +5,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open current file directory" })
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
@@ -16,19 +16,17 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Invert `:` and `;` in normal and visual modes
-vim.keymap.set('n', ';', ":")
-vim.keymap.set('n', ':', ";")
-vim.keymap.set('v', ';', ":")
-vim.keymap.set('v', ':', ";")
+vim.keymap.set({'n', 'v'}, ';', ":")
+vim.keymap.set({'n', 'v'}, ':', ";")
 
 -- Allow `'` to act like `\`` in normal mode
 vim.keymap.set('n', "'", "`")
 
 -- Easy moves between windows
-vim.keymap.set('n', '<C-h>', "<C-w>h")
-vim.keymap.set('n', '<C-j>', "<C-w>j")
-vim.keymap.set('n', '<C-k>', "<C-w>k")
-vim.keymap.set('n', '<C-l>', "<C-w>l")
+vim.keymap.set('n', '<C-h>', "<C-w>h", { desc = "Move to left Window" })
+vim.keymap.set('n', '<C-j>', "<C-w>j", { desc = "Move to lower Window" })
+vim.keymap.set('n', '<C-k>', "<C-w>k", { desc = "Move to upper Window" })
+vim.keymap.set('n', '<C-l>', "<C-w>l", { desc = "Move to right Window" })
 
 -- Easy moves when using terms
 vim.keymap.set('t', '<C-h>', "<C-\\><C-N><C-w>h")
@@ -37,25 +35,23 @@ vim.keymap.set('t', '<C-k>', "<C-\\><C-N><C-w>k")
 vim.keymap.set('t', '<C-l>', "<C-\\><C-N><C-w>l")
 
 -- Easy resize windows
-vim.keymap.set('n', '<C-left>', ':vertical resize -5<CR>')
-vim.keymap.set('n', '<C-down>', ':resize +5<CR>')
-vim.keymap.set('n', '<C-up>', ':resize -5<CR>')
-vim.keymap.set('n', '<C-right>', ':vertical resize +5<C')
+vim.keymap.set('n', '<C-left>', ':vertical resize -5<CR>', { desc = "Resize smaller horizontally" })
+vim.keymap.set('n', '<C-down>', ':resize +5<CR>', { desc = "Resize bigger vertically" })
+vim.keymap.set('n', '<C-up>', ':resize -5<CR>', { desc = "Resize smaller vertically" })
+vim.keymap.set('n', '<C-right>', ':vertical resize +5<C', { desc = "Resize bigger horizontally" })
 
 -- Old habits
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'jj', '<Esc>', { desc = "Escape" })
 
 -- There are other worlds
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', 'gg"+yG')
-vim.keymap.set('x', '<leader>p', '"_dP')
-vim.keymap.set('n', '<leader>d', '"_d')
-vim.keymap.set('v', '<leader>d', '"_d')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = "[Y]ank to other worlds" })
+vim.keymap.set('n', '<leader>Y', 'gg"+yG', { desc = "[Y]ank buffer to other worlds" })
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = "[P]aste to the void" })
+vim.keymap.set('n', '<leader>d', '"_d', { desc = "[D]elete to the void" })
 
 -- Move lines in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = "Move line down visually" })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = "Move line up visually" })
 
 -- `J` without moving cursor
 vim.keymap.set('n', 'J', 'mzJ`z')
@@ -68,21 +64,22 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- Avoid `Q`
 vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', 'C-f', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+
+vim.keymap.set('n', 'C-f', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { desc = "tmux-sessionizer - New Window" })
 
 -- Search and replace word current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and Replace current word" })
 
 -- Make current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
 
 -- Show keymaps
-vim.keymap.set("n", "<leader>tk", ":Telescope keymaps<CR>")
+vim.keymap.set("n", "<leader>tk", ":Telescope keymaps<CR>", { desc = "[T]elescope - [K]eymaps" })
 
 -- Choose YAML schema
-vim.keymap.set("n", "<leader>fy", ":Telescope yaml_schema<CR>")
+vim.keymap.set("n", "<leader>ty", ":Telescope yaml_schema<CR>", { desc = "[T]elescope - [Y]AML schema" })
 
--- Might be useful at some point, keep in mind
+-- NOTE: Might be useful at some point, keep in mind
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
