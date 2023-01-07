c7b48ba70012850a23f63840d88b5792294b0870
Julien Sudan <nedsi@pm.me>
2023-01-07 23:25:18 +0100

    fix: move todo-comment keymap at the right place

--- nvim/after/plugin/todo-comments.lua
+++ nvim/after/plugin/todo-comments.lua
@@ -1,4 +1,7 @@
 require("todo-comments").setup()
+
+vim.keymap.set('n', '<leader>sn', '<cmd>TodoTelescope<CR>', { desc = "Telescope - [S]earch [N]otes and Todos" })
+
 -- examples:
 -- WARN:
 -- FIX: 
