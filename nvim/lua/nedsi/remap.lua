bd603f3ea2afa1a5e35cceebe85c4889a3f35ea9
Julien Sudan <nedsi@pm.me>
2023-01-07 23:24:50 +0100

    fix: remove unnecessary note

--- nvim/lua/nedsi/remap.lua
+++ nvim/lua/nedsi/remap.lua
@@ -1,7 +1,6 @@
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
