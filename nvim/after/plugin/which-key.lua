f96fdb86a6ad104e55a1c187ca5f0ed978adfcd3
Julien Sudan <nedsi@pm.me>
2023-01-07 23:00:27 +0100

    fix: remaps with <leader>s

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -1,3 +1,3 @@
 require("which-key").setup() -- TODO: SET THIS UP! Complements well native descriptions
 
-vim.keymap.set('n', '<leader>tt', '<cmd>TodoTelescope<CR>', { desc = "[T]elescope [T]odo" })
+vim.keymap.set('n', '<leader>sn', '<cmd>TodoTelescope<CR>', { desc = "Telescope - [S]earch [N]otes and Todos" })
