09fec8547d612bd48ae3e74c322a4c9ebb6b9416
Julien Sudan <nedsi@pm.me>
2023-01-07 23:25:18 +0100

    fix: move todo-comment keymap at the right place

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -1,3 +1,1 @@
-require("which-key").setup() -- TODO: SET THIS UP! Complements well native descriptions
-
-vim.keymap.set('n', '<leader>sn', '<cmd>TodoTelescope<CR>', { desc = "Telescope - [S]earch [N]otes and Todos" })
+require("which-key").setup()
