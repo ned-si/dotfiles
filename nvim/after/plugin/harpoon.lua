6adccc438aec1bcb8425d3f88ea43d6ba059d905
Julien Sudan <nedsi@pm.me>
2023-01-08 22:43:40 +0100

    feat: change main harpoon remap to avoid collision with which-key for hunks

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon - [A]dd file" })
-vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "[H]arpoon" })
+vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { desc = "[HA]rpoon" })
 
 vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end, { desc = "Harpoon - File 1" })
 vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end, { desc = "Harpoon - File 2" })
