3df166d0b6a90b7c7b1e401e73fd4876233baec3
Julien Sudan <nedsi@pm.me>
2023-01-09 17:23:21 +0100

    feat: ltex and sessions now centralized

--- nvim/after/plugin/ltex.lua
+++ nvim/after/plugin/ltex.lua
@@ -3,7 +3,7 @@ require'lspconfig'.ltex.setup {
     require("ltex_extra").setup{
         load_langs = { "fr-CH", "en-US" }, -- table <string> : languages for witch dictionaries will be loaded
         init_check = true, -- boolean : whether to load dictionaries on startup
-        path = nil, -- string : path to store dictionaries. Relative path uses current working directory
+        path = "/home/nedsi/.config/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
         log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
     }
   end,
