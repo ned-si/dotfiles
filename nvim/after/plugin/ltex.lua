21537f6a2ff419c6ed70d981de3bcd80370cc22d
Julien Sudan <nedsi@pm.me>
2023-01-10 23:22:02 +0100

    feat: new sensible path for ltex dictionnaries and add en-GB dictionnary

--- nvim/after/plugin/ltex.lua
+++ nvim/after/plugin/ltex.lua
@@ -1,9 +1,9 @@
 require'lspconfig'.ltex.setup {
   on_attach = function(client, bufnr)
     require("ltex_extra").setup{
-        load_langs = { "fr-CH", "en-US" }, -- table <string> : languages for witch dictionaries will be loaded
+        load_langs = { "fr-CH", "en-US", "en-GB" }, -- table <string> : languages for witch dictionaries will be loaded
         init_check = true, -- boolean : whether to load dictionaries on startup
-        path = "/home/nedsi/.config/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
+        path = "/home/nedsi/.local/share/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
         log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
     }
   end,
