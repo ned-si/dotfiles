a66b3a7e4bab6bc469b6b48d406fae7262175782
Julien Sudan <nedsi@pm.me>
2023-01-12 21:24:50 +0100

    fix: more sensible harpoon keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon - [A]dd file" })
-vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { desc = "[HA]rpoon" })
+vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "[H]arpoon - [A]dd file" })
+vim.keymap.set("n", "<leader>hg", ui.toggle_quick_menu, { desc = "[H]arpoon [G]" })
 
 vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end, { desc = "Harpoon - File 1" })
 vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end, { desc = "Harpoon - File 2" })
