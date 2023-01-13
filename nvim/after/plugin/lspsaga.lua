00b9bc76a1ef995389bb0992e74da70c5e8a71da
Julien Sudan <nedsi@pm.me>
2023-01-13 21:51:16 +0100

    fix: lspsaga after update with broken changes

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -1,6 +1,4 @@
-local saga = require('lspsaga')
-
-saga.init_lsp_saga({
+require('lspsaga').setup({
   definition_action_keys = {
     edit = '<C-c>o', -- [o]pen
     vsplit = '<C-c>v',
@@ -14,10 +12,10 @@ saga.init_lsp_saga({
 -- if there is no implement it will hide
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump backlspsaga
-vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { desc = 'LSP: [G]et [H]elp', silent = true })
+vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { desc = 'LSP: [G]et [H]elp' })
 
 -- Code action
-vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = 'LSP: [C]ode [A]ction', silent = true })
+vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = 'LSP: [C]ode [A]ction' })
 
 -- Rename - replaced by inc-rename
 -- vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
@@ -26,14 +24,14 @@ vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc =
 -- you can edit the definition file in this flaotwindow
 -- also support open/vsplit/etc operation check definition_action_keys
 -- support tagstack C-t jump back
-vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { desc = 'LSP: [G]oto [D]efinition', silent = true })
+vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { desc = 'LSP: [G]oto [D]efinition' })
 
 -- Show cursor diagnostics
-vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { desc = 'LSP: show [C]urrent [D]iagnostic', silent = true })
+vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { desc = 'LSP: show [C]urrent [D]iagnostic' })
 
 -- Diagnostic jump can use `<c-o>` to jump back
-vim.keymap.set("n", "<C-p>", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "LSP: Diagnostic [P]revious", silent = true })
-vim.keymap.set("n", "<C-n>", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "LSP: Diagnostic [N]ext", silent = true })
+vim.keymap.set("n", "<C-p>", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "LSP: Diagnostic [P]revious" })
+vim.keymap.set("n", "<C-n>", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "LSP: Diagnostic [N]ext" })
 
 -- Only jump to error
 vim.keymap.set("n", "[E", function()
@@ -44,7 +42,7 @@ vim.keymap.set("n", "]E", function()
 end, { silent = true })
 
 -- Outline
-vim.keymap.set("n","<leader>o", "<cmd>Lspsaga outline<CR>",{ desc = "LSP: Show [O]utline", silent = true })
+vim.keymap.set("n","<leader>o", "<cmd>Lspsaga outline<CR>",{ desc = "LSP: Show [O]utline" })
 
 -- Hover Doc
-vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = 'Hover Documentation', silent = true })
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = 'Hover Documentation' })
