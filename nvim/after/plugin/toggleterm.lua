d679dcc282d77ba15cd419463dc1741354ab9e4d
Julien Sudan <nedsi@pm.me>
2023-01-23 11:06:29 +0100

    feat: new remaps for terminals

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -2,3 +2,6 @@ require("toggleterm").setup({
   direction = 'horizontal',
   open_mapping = [[<C-t>]],
 })
+
+vim.keymap.set("n", "<leader>ta", ":ToggleTermToggleAll<CR>", { desc = "[T]oggleTerm [A]all" })
+vim.keymap.set({"n", "v"}, "<leader>tv", ":ToggleTermSendVisualSelection<CR>", { desc = "[T]oggleTerm send [V]isual selection" })
