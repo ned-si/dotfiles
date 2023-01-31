c6762f28c94e1af7c1adb1cdd1ccfe50fd4eafa3
Julien Sudan <nedsi@pm.me>
2023-01-31 13:24:01 +0100

    fix: ltex allowing `$HOME` variable

--- nvim/after/plugin/ltex.lua
+++ nvim/after/plugin/ltex.lua
@@ -3,7 +3,7 @@ require'lspconfig'.ltex.setup {
     require("ltex_extra").setup{
         load_langs = { "fr-CH", "en-US", "en-GB" }, -- table <string> : languages for witch dictionaries will be loaded
         init_check = true, -- boolean : whether to load dictionaries on startup
-        path = "/home/nedsi/.local/share/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
+        path = "$HOME/.local/share/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
         log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
     }
   end,
