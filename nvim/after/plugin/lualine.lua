9db6287e73f19a640f1777f4c6a68c251f3a5efa
Julien Sudan <nedsi@pm.me>
2023-02-03 21:34:33 +0100

    feat: add word count to lualine in markdown and text files

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,20 @@
 -- Set lualine as statusline
 -- See `:help lualine.txt`
+
+local function getWords()
+  if vim.bo.filetype == "md" or vim.bo.filetype == "txt" or vim.bo.filetype == "markdown" then
+    if vim.fn.wordcount().visual_words == 1 then
+      return tostring(vim.fn.wordcount().visual_words) .. " word"
+    elseif not (vim.fn.wordcount().visual_words == nil) then
+      return tostring(vim.fn.wordcount().visual_words) .. " words"
+    else
+      return tostring(vim.fn.wordcount().words) .. " words"
+    end
+  else
+    return ""
+  end
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -7,4 +22,7 @@ require('lualine').setup {
     component_separators = '|',
     section_separators = '',
   },
+  sections = {
+    lualine_a = { getWords }
+  }
 }
