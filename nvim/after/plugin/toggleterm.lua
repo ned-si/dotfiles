20afa92b0a6e04f70670cf206459972c45eb0957
Julien Sudan <nedsi@pm.me>
2023-02-14 14:28:31 +0100

    feat: toggleterm now vertical with adjusted sizes

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,7 +1,15 @@
 require("toggleterm").setup({
-  direction = 'horizontal',
+  direction = 'vertical',
   open_mapping = [[<C-t>]],
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return 111
+    end
+  end,
 })
 
 vim.keymap.set("n", "<leader>ta", ":ToggleTermToggleAll<CR>", { desc = "[T]oggleTerm [A]all" })
-vim.keymap.set({"n", "v"}, "<leader>tv", ":ToggleTermSendVisualSelection<CR>", { desc = "[T]oggleTerm send [V]isual selection" })
+vim.keymap.set({ "n", "v" }, "<leader>tv", ":ToggleTermSendVisualSelection<CR>",
+  { desc = "[T]oggleTerm send [V]isual selection" })
