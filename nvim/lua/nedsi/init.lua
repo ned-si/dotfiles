1c32d0f83ec0501ccacfed6b3706293363b2119b
Julien Sudan <nedsi@pm.me>
2023-02-20 00:07:31 +0100

    fix: remove default spell check for markdown

--- nvim/lua/nedsi/init.lua
+++ nvim/lua/nedsi/init.lua
@@ -20,7 +20,6 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.opt_local.textwidth = 80
     vim.opt_local.wrap = true
-    vim.opt_local.spell = true
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
     vim.opt_local.shiftwidth = 2
