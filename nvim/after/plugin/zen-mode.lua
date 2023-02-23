63a1928abc059433b24a02941c22c79a41f344cc
Julien Sudan <nedsi@pm.me>
2023-02-23 09:28:52 +0100

    fix: remove font size change in zen-mode

--- nvim/after/plugin/zen-mode.lua
+++ nvim/after/plugin/zen-mode.lua
@@ -30,8 +30,12 @@ require("zen-mode").setup({
     twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = true }, -- disables git signs
     tmux = { enabled = true }, -- disables the tmux statusline
+    -- this will change the font size on kitty when in zen mode
+    -- to make this work, you need to set the following kitty options:
+    -- - allow_remote_control socket-only
+    -- - listen_on unix:/tmp/kitty
     kitty = {
-      enabled = true,
+      enabled = false,
       font = "+10", -- font size increment
     },
     -- this will change the font size on alacritty when in zen mode
