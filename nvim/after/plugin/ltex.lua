d4d6eb37618e6ea2fe47e5ef401d7b2728351101
Julien Sudan <nedsi@pm.me>
2023-03-03 21:53:00 +0100

    fix: indentation in ltex conf

--- nvim/after/plugin/ltex.lua
+++ nvim/after/plugin/ltex.lua
@@ -1,10 +1,10 @@
-require'lspconfig'.ltex.setup {
+require 'lspconfig'.ltex.setup {
   on_attach = function(client, bufnr)
-    require("ltex_extra").setup{
-        load_langs = { "fr-CH", "en-US", "en-GB" }, -- table <string> : languages for witch dictionaries will be loaded
-        init_check = true, -- boolean : whether to load dictionaries on startup
-        path = "$HOME/.local/share/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
-        log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+    require("ltex_extra").setup {
+      load_langs = { "fr-CH", "en-US", "en-GB" }, -- table <string> : languages for witch dictionaries will be loaded
+      init_check = true, -- boolean : whether to load dictionaries on startup
+      path = "$HOME/.local/share/nvim/.ltex", -- string : path to store dictionaries. Relative path uses current working directory
+      log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
     }
   end,
   settings = {
