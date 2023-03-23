8ec59da98ad9794a942dc4773bbcc98a416dfabf
Julien Sudan <nedsi@pm.me>
2023-03-23 23:21:48 +0100

    feat(nvim): new plugin - copilot! remove unused conflicting LSP remap

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -105,7 +105,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -114,7 +114,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-p>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
