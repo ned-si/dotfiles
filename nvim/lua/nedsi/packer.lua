af301225202bb16a18cef9b3dcb8bce3fcf9dc70
Julien Sudan <nedsi@pm.me>
2023-03-28 08:43:31 +0200

    feat(nvim): new plugin to enhance `w`, `b`, `e` and `ge` moves: nvim-spider

--- nvim/lua/nedsi/packer.lua
+++ nvim/lua/nedsi/packer.lua
@@ -83,6 +83,8 @@ require('packer').startup(function(use)
       require("various-textobjs").setup({ useDefaultKeymaps = true })
     end,
   }
+  -- better movements
+  use { "chrisgrieser/nvim-spider" }
 
   -- Git related plugins
   use 'tpope/vim-fugitive'
