d74e67d77937f461e6da16530aa427a208236883
Julien Sudan <nedsi@pm.me>
2023-07-11 15:43:37 +0200

    chore(nvim): packer stuff

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -495,14 +495,14 @@ time([[Defining packer_plugins]], false)
 time([[Config for nvim-various-textobjs]], true)
 try_loadstring("\27LJ\2\nX\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\22useDefaultKeymaps\2\nsetup\21various-textobjs\frequire\0", "config", "nvim-various-textobjs")
 time([[Config for nvim-various-textobjs]], false)
--- Config for: yaml-companion.nvim
-time([[Config for yaml-companion.nvim]], true)
-try_loadstring("\27LJ\2\nP\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\16yaml_schema\19load_extension\14telescope\frequire\0", "config", "yaml-companion.nvim")
-time([[Config for yaml-companion.nvim]], false)
 -- Config for: advanced-git-search.nvim
 time([[Config for advanced-git-search.nvim]], true)
 try_loadstring("\27LJ\2\nX\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\24advanced_git_search\19load_extension\14telescope\frequire\0", "config", "advanced-git-search.nvim")
 time([[Config for advanced-git-search.nvim]], false)
+-- Config for: yaml-companion.nvim
+time([[Config for yaml-companion.nvim]], true)
+try_loadstring("\27LJ\2\nP\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\16yaml_schema\19load_extension\14telescope\frequire\0", "config", "yaml-companion.nvim")
+time([[Config for yaml-companion.nvim]], false)
 -- Config for: template.nvim
 time([[Config for template.nvim]], true)
 try_loadstring("\27LJ\2\nR\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\18find_template\19load_extension\14telescope\frequire\0", "config", "template.nvim")
