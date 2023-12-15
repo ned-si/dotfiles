dbc7b5c92f28b1e2f4e7e5fa504d14d7341ba70c
Julien Sudan <nedsi@pm.me>
2023-12-15 22:58:48 +0100

    feat(nvim): remove dressing because of unknown error with Code Action window

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false }, -- NOTE: disabled afer isssue with Code Action window. Work with default LazyVim config.
 }
