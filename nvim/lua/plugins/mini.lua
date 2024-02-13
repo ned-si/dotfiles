7d10c9b40d50b2770e65fc6f25c365965c269805
Julien Sudan <nedsi@pm.me>
2024-02-13 10:33:54 +0100

    feat(nvim): new `mini.lua` plugin to move around and modify file trees as regular buffers

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,4 +20,23 @@ return {
     "echasnovski/mini.align",
     version = false,
   },
+  {
+    "echasnovski/mini.trailspace",
+    version = false,
+    config = function()
+      require("mini.trailspace").setup()
+    end,
+    keys = {
+      {
+        "<leader>tw",
+        "<cmd>lua MiniTrailspace.trim()<cr>",
+        desc = "[T]rim [W]hitespaces",
+      },
+      {
+        "<leader>tl",
+        "<cmd>lua MiniTrailspace.trim_last_lines()<cr>",
+        desc = "[T]rim [L]ast lines",
+      },
+    },
+  },
 }
