1e717cbd5198408fe787c940821788ae7366fd62
Julien Sudan <nedsi@pm.me>
2024-02-13 10:35:01 +0100

    feat(nvim): disable headlines plugin (for markdown)

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "stevearc/dressing.nvim", enabled = false }, -- NOTE: disabled afer isssue with Code Action window. Work with default LazyVim config.
+  { "lukas-reineke/headlines.nvim", enabled = false },
 }
